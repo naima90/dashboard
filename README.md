6f47bfe132aee2e68e99a624f170a8015b8f7067
naima <naimaosman203@gmail.com>
2022-06-06 11:18:38 +0100

    server to server request setup & updated README.md

---
+++ README.md
@@ -0,0 +1,29 @@
+## Description
+
+A dashboard that makes it easy for for organisers to view race startlist entries and ticket sale revenue.
+
+###  Stack:
+
+Using React.js for the client-side.
+Using express for the server-side to make a request to the api.  
+
+## To run locally
+
+###  Before running:
+
+1. Clone this repo 
+
+2. cd server & client in split terminal
+
+3. In command line:
+
+```
+npm install
+npm start
+```
+
+The server side:
+http://localhost:5000
+
+The client side:
+http://localhost:3000
--- server/server.js
+++ server/server.js
@@ -1,14 +1,32 @@
 const express = require('express');
+const axios = require('axios');
+const cors = require("cors");
+
 const port = 5000;
 
-// initialize app
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+
+// handles cross-origin requests
+app.use(cors({
+  origin: "*",
+}));
+
+// getting data from LDT api & returning it.
+app.get("/api/startlistentries", async (req, res) => {
+  await axios.get('https://ldt-tech-test.herokuapp.com/api/startlistentries')
+  .then((response) => {
+    console.log(response.data[0]);
+    res.send(response.data)
+  })
+  .catch((err) =>{
+    console.log(err);
+  });
 });
+
+
 
 // logs what port server is running on
 app.listen(port,() => {
-  console.log(`From port ${port}`);
+    console.log(`From port ${port}`);
 });
